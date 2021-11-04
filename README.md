cf7da0233373b9ed761039e3600b187cf2e6c44c
“GiseleTrizotte” <gntsilva.gl@gmail.com>
2021-11-04 20:39:40 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gostack_gobarber
+
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,35 @@
+import startOfHour from "date-fns/startOfHour";
+import { getCustomRepository } from "typeorm";
+import Appointment from "../models/Appointment";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+
+interface RequestCreateAppointment {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+
+  public async execute({ date, provider_id }: RequestCreateAppointment): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentsInSameDate =
+      await appointmentsRepository.findByDate(appointmentDate);
+
+    if (findAppointmentsInSameDate) {
+      throw Error("This appointment is already booked");
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
