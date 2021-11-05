48f7d0f088b5fde980be3b3db777437ca3dff468
Gisele Trizotte <57105956+GiseleTrizotte@users.noreply.github.com>
2021-11-05 18:39:50 -0300

    feature: token jwt (#2)
    Co-authored-by: “GiseleTrizotte” <“gntsilva.gl@gmail.com”>

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,9 +1,8 @@
-import { EntityRepository, Repository } from "typeorm";
-import Appointment from "../models/Appointment";
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../models/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>{
-
+class AppointmentsRepository extends Repository<Appointment> {
   public async findByDate(date: Date): Promise<Appointment | null> {
     const findAppointments = await this.findOne({
       where: { date },
